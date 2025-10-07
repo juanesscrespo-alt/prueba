18d4d041abaa1dcb33d0d85efa9628fba8d12fc6
MORERAPOWER.JR <juaness.crespo@gmail.com>
2025-10-07 14:11:31 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,116 @@
+# Windows 2019 CIS audit
+
+## Overview
+
+**This is a BETA release.**
+
+### How it works
+
+- The audit is designed to run as part of the ansible remediation playbook (coming soon) or as a standalone configurable script contained within this repo (run_audit.ps1)
+  - This script discovers and sets several variables to ensure consistent running of the command.
+  - This also allows the audit to be triggered by other automations
+- When goss runs it will run the required OS commands to capture the data for analysis.
+  - For GPO settings goss runs the powershell script ./scripts/gpo_regex.ps1 with arguments is run to search for the matching policy name
+  - Will output the details if defined
+  - if nothing is found will output "Not Defined"
+
+#### NOTE: It is expected to run from a single audit directory (a directory containing both the goss file and audit profile), you will need to modify the script paths in vars accordingly
+
+### What is goss?
+
+Gives the ability to audit a local system using a lightweight binary to check the current state.
+
+This is:
+
+- very small 11MB executable
+- low resource impact
+- self contained
+
+Due to the variations that can occur within windows this is released as beta.
+It has been tested on base installation
+
+- standalone system
+- domain controller
+
+[How To Guide](Docs/Security_remediation_and_auditing.MD)
+
+### Development/Contributing Notes
+
+- goss.yml - the main goss file to run (has to be used with a -g) - this loads all the sections as required
+- (benchmark_name).yml - These are the variable used as part of the goss file - this is split into sections to control the variables - This file will get large
+- Try to reuse elements/vars as much as possible
+- use variables wherever you can to be more efficient in the code
+- Build variables up
+- Some controls only work on DC or MS - The settings in Vars will determine if host is DC or MS (will be populated by ansible when run from task)
+- some controls written twice, this is due to different vars for a DC or MS (e.g. 2.2.7)
+
+## Join us
+
+On our [Discord Server](https://discord.gg/JFxpSgPFEJ) to ask questions, discuss features, or just chat with other Ansible-Lockdown users
+
+## Requirements
+
+- Permissions to run all the commands may need admin to run this
+  - also if iis or exchange is installed
+
+- download goss (current version 0.3.6 - Alpha for windows)
+  - [x86_64-goss](https://github.com/aelsabbahy/goss/releases/download/v0.3.goss-alpha-windows-amd64.exe)
+  - validate SHA
+
+- Suggest reboot and gpupdate is run prior to audit - will potentially give differing results
+
+## Not using wrapper script information
+
+These are just some of the requirements needed if running goss standalone.
+Please refer to goss documentation if running manually.
+
+- Goss to be on the host running the audit _ note its current alpha but works well
+  - need to set environment
+
+```txt
+$env:GOSS_USE_ALPHA=1
+```
+
+### Domain members and domain controllers
+
+- gpresult /v /r > file_location.txt need to be created (variable gpresult_file  needs to be updated)
+- auditpol.exe /get /category:* > file_location.txt ( the variable auditresults_file needs to be updated)
+
+### If standalone server will require the following commands
+
+- secedit /export /cfg {{ file output location }} ( variable standalone_policies.txt )
+- auditpol.exe /get /category:* > file_location.txt
+- Due to the output we need to search for SID for std users using the MS doc below
+  - [Microsoft_security_identifiers](https://docs.microsoft.com/en-us/troubleshoot/windows-server/identity/security-identifiers-in-windows)
+  - also added to vars for completeness
+
+## further information
+
+- [goss documentation](https://github.com/aelsabbahy/goss/blob/master/docs/manual.md#patterns)
+- [CIS standards](https://www.cisecurity.org)
+
+## Example output (standalone after running benchmark)
+
+```ps
+PS C:\vagrant\Win2019-CIS-Audit> .\run_audit.ps1
+Pre checks - Ensure files exist
+OK - "C:\vagrant\goss.exe" exists
+OK - "C:\vagrant\Win2019-CIS-Audit\CIS.yml" exists
+OK - "C:\vagrant\Win2019-CIS-Audit\goss.yml" exists
+OK - Files Exist
+Running Audit commands
+
+OK - ran auditpol report - created C:\vagrant\auditpol_1646394033.txt
+
+StandAlone Server system discovered running relevant checks
+
+OK - secedit report - created C:\vagrant\secedit_1646394033.txt
+
+Running Audit
+Audit Successful
+
+    "summary": {         "failed-count": 31,         "summary-line": "Count: 661, Failed: 31, Duration: 44.994s",         "test-count": 661,         "total-duration": 44993809300     } }
+
+Complete audit file can be found at C:\vagrant\audit_1646394033.json
+PS C:\vagrant\Win2019-CIS-Audit>
+```
---
+++ run_audit.ps1
@@ -0,0 +1,318 @@
+# Initial Powershell script
+# 04/03/2022 - Beta release - ukbolly mindpointgroup
+# 07/03/2022 - Extended cli options and docs
+
+
+<#
+.SYNOPSIS
+Wrapper script to run an audit
+
+.DESCRIPTION
+Wrapper script to run an audit on the system using goss.
+This allows for bespoke variables to be set
+
+
+.PARAMETER auditdir
+default: $DEFAULT_CONTENT_DIR
+Ability to change the location of where the content can be found
+This is where the audit content is stored
+e.g. c:/windows_audit
+
+.PARAMETER binpath
+default: $DEFAULT_AUDIT_BIN
+Ability to change the location of where the audit binary can be found
+This is where the audit content is stored
+e.g. c:/$DEFAULT_CONTENT_DIR/goss.exe
+
+.PARAMETER varsfile
+default: $DEFAULT_VARS_FILE
+Ability to set a variable file defined with the settings to match your requirements
+
+.PARAMETER group
+default: none
+Ability to set a group that the system belongs to
+Can be used when matching similar system in that same group
+
+.PARAMETER outfile
+default: $AUDIT_CONTENT_DIR\audit_$host_os_hostname_$host_epoch.json
+Ability to set an outfile to send the full audit output to
+Requires path to be set.
+e.g. c:/windows_audit_reports
+
+
+.EXAMPLE
+./run_audit.ps1
+./run_audit.ps1 -auditbin c:\path_to\binary.name
+./run_audit.ps1 -auditdir c:\somepath_for _audit_content
+./run_audit.ps1 -varsfile myvars.yml
+./run_audit.ps1 -outfile path\to\audit\output.json
+./run_audit.ps1 -group webserver
+
+.NOTES
+
+
+.LINK
+https://github.com/ansible-lockdown/Windows-2016-CIS-Audit/blob/devel/Docs/Security_remediation_and_auditing.md
+
+#>
+
+param (
+      [string]$auditbin,
+      [string]$auditdir,
+      [string]$varsfile,
+      [string]$group,
+      [string]$outfile
+)
+
+
+## Variables
+
+# Benchmark variables
+$BENCHMARK = "CIS"
+$BENCHMARK_VER  = "1.2.1"
+$BENCHMARK_OS   = "Windows 2019"
+
+# Set Variables for Audit
+
+$DEFAULT_CONTENT_DIR = "C:\scripts"  # This can be changed using cli
+$DEFAULT_VARS_FILE = "$BENCHMARK.yml"  # This can be changed using cli option
+$DEFAULT_AUDIT_BIN = "$DEFAULT_CONTENT_DIR\goss.exe"  # This can be changed using cli option
+
+### Shouldn't need to change anything past this point apart from tidy up the code
+$AUDIT_CONTENT_VERSION = "Windows-2016-$BENCHMARK-Audit"
+$DEFAULT_AUDIT = "goss.yml"
+
+
+# $binpath
+if ([string]::IsNullOrEmpty($auditbin)){
+    $AUDIT_BIN = "$DEFAULT_AUDIT_BIN"
+    }
+else {
+    $AUDIT_BIN = "$auditbin"
+    }
+
+# $auditdir
+if ([string]::IsNullOrEmpty($auditdir)){
+    $AUDIT_CONTENT_DIR = "$DEFAULT_CONTENT_DIR"
+    }
+else {
+    $AUDIT_CONTENT_DIR = "$auditdir"
+    }
+
+$AUDIT_CONTENT_FULLPATH = "$AUDIT_CONTENT_DIR\$AUDIT_CONTENT_VERSION"
+
+
+# $varsfile
+if ([string]::IsNullOrEmpty($varsfile)){
+    $AUDIT_VARS = "$DEFAULT_VARS_FILE"
+    }
+else {
+    $AUDIT_VARS = "$varsfile"
+    }
+
+
+# Check file exists
+
+$files = @( "$AUDIT_BIN", "$AUDIT_CONTENT_FULLPATH\$AUDIT_VARS", "$AUDIT_CONTENT_FULLPATH\$DEFAULT_AUDIT" )
+
+Write-host "Pre checks - Ensure files exist"
+foreach ($file in $files) {
+  try {
+     if (Test-Path -Path $file){
+         write-host "OK - ""$file"" exists" -ForeGroundColor green
+     }
+     else{
+          $file_missing = 'true'
+          throw
+     }  
+  }
+  catch {
+     Write-host "Fail - ""$file"" Not found" -ForegroundColor red
+     
+  }
+}
+
+if ($file_missing){
+    Write-Warning "Please Ensure variables are set correctly"
+    Exit
+    }
+else{
+    Write-Host "OK - Files Exist" -ForegroundColor green
+}
+
+# Allow Alpha version to run
+# Until 0.4 is official when not required.
+$env:GOSS_USE_ALPHA=1
+$env:GOSS_TIMEOUT=10
+
+
+# Check content exists before continuing
+
+
+<# 
+Determine Server type -e.g. standalone, domain member , PDC etc
+ 2 StandAlone Server
+ 3 Member Server
+ 4 Backup Domain Controller
+ 5 Primary Domain Controller
+#>
+$serverrole = (wmic.exe ComputerSystem get DomainRole | Select-String -Pattern "[0-9]")
+$servertype = ($serverrole) -replace '\D+(\d+)','$1'
+
+# Epoch time is required (as per Unix based from UTC)
+$host_audit_time = ([Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s")))
+
+# Output files
+$gpresult_file="$AUDIT_CONTENT_DIR\gpresult_$host_audit_time.txt"
+$auditresult_file="$AUDIT_CONTENT_DIR\auditpol_$host_audit_time.txt"
+$secedit_file="$AUDIT_CONTENT_DIR\secedit_$host_audit_time.txt"
+
+# $group - ability to add a group to the metadata to compare same group server results
+if ([string]::IsNullOrEmpty($group)){
+    $auto_group = "ungrouped"
+    }
+else {
+    $auto_group = "$group"
+    }
+
+
+# Run Required commands based upon server type to capture system information to be audited
+# Run audit policies capture script - required for all types
+
+$Error.Clear()
+
+Write-Host "Running Audit commands"
+
+$audit_cmd = Invoke-Command -Script { auditpol.exe /get /category:* > $auditresult_file } -ErrorAction SilentlyContinue
+
+Try {
+      "$audit_cmd" 
+}
+Catch {  
+}
+
+If ($LASTEXITCODE -ne "0"){
+    Write-Host "Unable to run auditpol.exe command"
+    exit
+}
+Else{
+    Write-Host "OK - ran auditpol report - created $auditresult_file" -ForegroundColor green
+    Write-Host ""
+}
+
+
+
+if ( $servertype = 2 )
+{
+  $OS_TYPE="StandAlone Server"
+  Write-Host "$OS_TYPE system discovered running relevant checks"
+  $secedit_cmd = Invoke-Command -Script { secedit.exe /export /cfg $secedit_file } -ErrorAction SilentlyContinue
+  Try {
+      "$secedit_cmd" 
+  }
+  Catch {
+  }
+  If ($LASTEXITCODE -ne "0"){
+    Write-Host "Unable to run secedit.exe command"
+    exit
+  }
+  Else{
+      Write-Host "OK - secedit report - created $secedit_file" -ForegroundColor green
+  }
+}
+Else 
+{
+      if ( $servertype = 3 )
+       {
+        $OS_TYPE="Member Server"
+       }
+      elif ( $servertype = 4 )
+       {
+        $OS_TYPE="Primary Domain Controller"
+       }
+      elif ( $servertype = 5 )
+       {
+        $OS_TYPE="Backup Domain Controller"
+       }
+      else {
+        $OS_TYPE="Workstation"
+       }
+  Write-Host "$OS_TYPE system discovered running relevant checks"
+  powershell.exe -noninteractive -noprofile -command gpresult /v /r $gpresult_file
+
+}
+
+# get metadata
+$host_os_name=((Get-CimInstance -ClassName CIM_OperatingSystem).Caption ) -replace ' ','_'
+$host_machine_uuid=(Get-CimInstance -Class Win32_ComputerSystemProduct).UUID
+$host_epoch=$host_audit_time
+$host_os_locale=((Get-TimeZone).Id) -replace ' ','_'
+$host_os_version=Get-WmiObject Win32_OperatingSystem | Select-Object -ExpandProperty Caption
+$host_os_hostname=(hostname)
+$host_system_type="$OS_TYPE"
+
+# allow audit_run variable to be run from external script providing different vars
+if ([string]::IsNullOrEmpty($audit_run)){
+    $audit_run="wrapper"
+}
+
+# Set the audit_content dir assist in fault finding due to go and windows \ / path differences
+
+$audit_content="$AUDIT_CONTENT_DIR"
+
+# Probably the ugliest thing ever with so much room to go wrong :)
+# Has to be a better way
+
+$AUDIT_JSON_VARS = "{ 'benchmark_type': `'$BENCHMARK`', 'benchmark_version': `'$BENCHMARK_VER`', 'benchmark_os': `'$BENCHMARK_OS`', 'machine_uuid': `'$host_machine_uuid`','os_deployment_type': `'$host_system_type`', 'epoch': `'$host_epoch`', 'os_locale': `'$host_os_locale`', 'os_release': `'$host_os_version`', 'host_os_distribution': `'$host_os_name`', 'os_hostname': `'$host_os_hostname`', 'auto_group': `'$auto_group`', 'gpresult_file': `'$$gpresult_file`', 'auditresult_file': `'$auditresult_file`', 'secedit_file': `'$secedit_file`','audit_content': `'$audit_content`'}"
+
+# Set up AUDIT_OUT
+#$outfile
+if ([string]::IsNullOrEmpty($outfile)){
+    $AUDIT_OUT = "$AUDIT_CONTENT_DIR\audit_$host_os_hostname_$host_epoch.json"
+    }
+else {
+    $AUDIT_OUT = "$outfile"
+    }
+
+$AUDIT_ERR = "$AUDIT_CONTENT_DIR\audit_$host_os_hostname_$host_epoch.err"
+
+# create empty file - dont output
+New-Item -ItemType file $AUDIT_OUT | Out-Null
+
+
+
+# run audit
+
+Write-Host "`nRunning Audit"
+# This runs the job, waits for its children to complete and outputs to file
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = "$AUDIT_BIN"
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.Arguments = "--gossfile $AUDIT_CONTENT_FULLPATH\$DEFAULT_AUDIT --vars $AUDIT_CONTENT_FULLPATH\$AUDIT_VARS --vars-inline `"$AUDIT_JSON_VARS`" v --format json --format-options pretty"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+$stdout = $p.StandardOutput.ReadToEnd()
+$stderr = $p.StandardError.ReadToEnd()
+$p.WaitForExit()
+
+# Write to relevant output to file
+Write-Output "$stdout" | Out-File -FilePath "$AUDIT_OUT"
+
+
+# Summary of Output
+
+if ( Select-String $BENCHMARK $AUDIT_OUT )
+    { 
+       
+       $audit_summary=Get-Content "$AUDIT_OUT" -tail 8
+       Write-Host "Audit Successful`n" -ForegroundColor green
+       Write-Host "$audit_summary"
+       Write-Host "`nComplete audit file can be found at $AUDIT_OUT"
+    }
+else
+    {
+       Write-Host "Fail Audit - There were issues when running the audit please investigate" 
+    }
